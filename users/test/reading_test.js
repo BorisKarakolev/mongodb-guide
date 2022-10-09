4e9acddc2b223bf9568877f23a84641e1f2542be
Boris Karakolev <boriskarakolev@hotmail.com>
2022-10-09 22:27:47 +0200

    Handling big collections with pagination

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -2,11 +2,17 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of the db", () => {
-  let joe;
+  let joe, maria, alex, bonnie;
 
   beforeEach((done) => {
+    alex = new User({ name: "Alex" });
+    maria = new User({ name: "Maria" });
+    bonnie = new User({ name: "Bonnie" });
     joe = new User({ name: "Joe" });
-    joe.save().then(() => done());
+
+    Promise.all([alex.save(), joe.save(), maria.save(), bonnie.save()]).then(
+      () => done()
+    );
   });
 
   it("finds all users with a name of joe", (done) => {
@@ -21,5 +27,18 @@ describe("Reading users out of the db", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  it("can skip and limit the result set", (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === "Bonnie");
+        assert(users[1].name === "Joe");
+        done();
+      });
   });
 });
